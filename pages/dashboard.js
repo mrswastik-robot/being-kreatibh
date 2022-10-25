1681997cdebae444ce4da22d6da84d4dc012c391
Swastik Patel <swastikpatel29@gmail.com>
2022-10-25 12:34:23 +0530

    initial

---
+++ pages/dashboard.js
@@ -0,0 +1,30 @@
+import {auth} from "../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { async } from "@firebase/util";
+
+export default function Dashboard()
+{
+    const route = useRouter();
+    const [user ,loading] = useAuthState(auth);
+
+    const getData = async() => {
+        if(loading) return;
+        if(!user) return route.push("/auth/login");
+    };
+
+    useEffect(() => {
+        getData();
+
+    }, [user, loading])
+
+
+    return(
+        <div>
+            <h1>Your Posts</h1>
+            <div>posts</div>
+            <button onClick={() => auth.signOut()}>Sign Out</button>
+        </div>
+    );
+}
