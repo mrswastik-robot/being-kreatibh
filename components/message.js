45bb1ebec39d0c5e7a9e37e11af582ef697b78b5
Swastik Patel <swastikpatel29@gmail.com>
2022-10-25 14:49:05 +0530

    ubuntu commit this is

--- components/message.js
+++ components/message.js
@@ -3,6 +3,7 @@ export default function Message()
     return(
         <div>
             <h1>ab ye kr k dikhaooo...</h1>
+            <h2>ye commit krte hue ubuntu k vs se...</h2>
             
         </div>
     )
